4d85d03f101e0a2ff34ffb8cdd58c968e8235725
Sebas-MD <109002441+Sebas-MD@users.noreply.github.com>
2023-07-11 12:52:04 -0500

    Constants.java

--- app/src/main/java/com/proyecto/bookapp/Constants.java
+++ app/src/main/java/com/proyecto/bookapp/Constants.java
@@ -1,5 +1,5 @@
 package com.proyecto.bookapp;
 
 public class Constants {
-    public static final String API_URL = "http://www.rogerhuauya.com/server";
+    public static final String API_URL = "http://www.rogerhuauya.com/server/";
 }
